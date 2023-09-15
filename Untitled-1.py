192bfa4d65b44571fd3a94745b3ad1b312068db6
John_Wick <misha_potapov_2004@mail.ru>
2023-09-15 11:23:22 +0400

    python 1

---
+++ Untitled-1.py
@@ -0,0 +1,12 @@
+#a = input("Ваше фамилия,имя? ")
+#b = input("Сколько вам лет?")
+#c= input("Где вы живёте?")
+#print("Ваше фамилия, имя: ",a)
+#print("Ваш возраст: ",b)
+#print("Вы живёте: ",c)
+
+x = 10
+t =1
+import math
+Z = round(((((9*math.pi*t)+(10*math.cos(x)))/(math.sqrt(t)-math.fabs(math.sin(t)))))*math.exp(x),2)
+print(Z)
